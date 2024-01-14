472502a374ddf3e7e44fb0bb78f3d6a5a27b593b
Annie <anam.k@live.com>
2024-01-14 18:59:06 +0000

    added all the data items required for the readme generator

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.title},
+  # ${data.title}
+
   ## Description
   ${data.description}
+  
   ## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
   ## Installation
-  ${data.installation} 
-`;
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  For questions, please contact me at [${data.email}](mailto:${data.email}).
+  GitHub Profile: [${data.github}](https://github.com/${data.github})
+  `;
 }
 
 module.exports = generateMarkdown;
